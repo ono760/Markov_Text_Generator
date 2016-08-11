225b0b7384d2d8f861a956f008394bf66a1013bc
Onofre Echeverria <ono.ucsb@gmail.com>
2016-08-11 11:48:47 -0700

    updated version

--- markov.py
+++ markov.py
@@ -40,7 +40,6 @@ class Markovify:
         self.tokens_speech = word_tokenize(self.corpus_speech)
         self.pos_speech = nltk.pos_tag(self.tokens_speech)
         self.words_speech = self.corpus_speech.split(' ')
-        # print(self.words_speech,"SPEEEECCCCHHH")
 
     def import_tweet(self):
         twitter = twython.Twython(CONSUMER_KEY,CONSUMER_SECRET)
@@ -55,8 +54,7 @@ class Markovify:
         self.tokens_tweet = word_tokenize(self.corpus_tweet)
         self.pos_tweet = nltk.pos_tag(self.tokens_tweet)
         self.words_tweet = self.corpus_tweet.split(' ')
-        # print (self.words_tweet, "TWEEEETTT")
-
+        
     def sentiment_filter(self, text_type):
         if self.sentiment == 'positive':
             sentiment_factor = .5
@@ -146,7 +144,7 @@ trump.import_speech('06-22-16-On_hilary.txt', 'speech')
 trump.import_tweet()
 trump.import_speech('08-08-16-2nd_amend_speech.txt', 'speech')
 trump.import_speech('07-28-16-RNC.txt', 'speech')
-trump.sentiment_filter('tweet')
+trump.sentiment_filter('speech')
 trump.create_dictionary()
 trump.generate_text()
 >>>>>>> 6ae1596... added def speech and def tweet methods
